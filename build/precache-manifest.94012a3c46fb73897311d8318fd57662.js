5b238e11f7b71e2ec5131c115539b27352d2148d
Kanji2137 <samuel.zabczynski@gmail.com>
2020-06-24 13:06:53 +0200

    ...

---
+++ build/precache-manifest.94012a3c46fb73897311d8318fd57662.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c898c8f47fe42265d4cd9e11b46933",
+    "url": "/index.html"
+  },
+  {
+    "revision": "508a249cdc9fa2e2def7",
+    "url": "/static/css/main.0fb05083.chunk.css"
+  },
+  {
+    "revision": "a0bc1b03202bb61b9369",
+    "url": "/static/js/2.e33d2518.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e33d2518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508a249cdc9fa2e2def7",
+    "url": "/static/js/main.f643b194.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7f2b4eddbb21ed8d07788fd4a965614c",
+    "url": "/static/media/blender_logo_socket2.7f2b4edd.png"
+  },
+  {
+    "revision": "9a6320021bc79c31a271e024b924beba",
+    "url": "/static/media/first1.9a632002.png"
+  },
+  {
+    "revision": "d85c7d7054c362372c0ab9d163589c4b",
+    "url": "/static/media/fourth.d85c7d70.png"
+  },
+  {
+    "revision": "a482678a028110c041198e3d2b343909",
+    "url": "/static/media/legoBoy1.a482678a.png"
+  },
+  {
+    "revision": "35cfc61f649201155cf0b1967c5a12bc",
+    "url": "/static/media/legoGuyGif2.35cfc61f.gif"
+  },
+  {
+    "revision": "219aa32733a247cc47d239ceb841ffd9",
+    "url": "/static/media/myInspirations.219aa327.png"
+  },
+  {
+    "revision": "bfb9b93d4194b93131bdb0e718cf041d",
+    "url": "/static/media/myportfolio.bfb9b93d.png"
+  },
+  {
+    "revision": "d04513b8b273cbcd000d64f2694382b9",
+    "url": "/static/media/second1.d04513b8.png"
+  },
+  {
+    "revision": "cca65ceb8f0b08ba828fe6caf6eadeca",
+    "url": "/static/media/third.cca65ceb.png"
+  }
+]);
